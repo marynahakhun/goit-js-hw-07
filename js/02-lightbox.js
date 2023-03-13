a782afe48471126614778013e8a4f89cf358bfd1
Maryna Hakhun <114681951+marynahakhun@users.noreply.github.com>
2023-03-13 15:06:27 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,47 +1,6 @@
-// import { galleryItems } from './gallery-items.js';
-
-// const galleryContainerEl = document.querySelector(".gallery");
-// const imagesMarkup = createItemsMarkup(galleryItems);
-
-// galleryContainerEl.addEventListener("click", onContainerClick);
-// galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
-
-// function createItemsMarkup(item) {
-//   return galleryItems
-//     .map(({ preview, original, description }) => {
-//       return `
-//       <li>
-//       <a class="gallery__item" href="${original}">
-//       <img class="gallery__image" 
-//       src="${preview}" 
-//       alt="${description}" />
-//         </a>
-//       </li>`
-   
-//     })
-//     .join("");
-// }
-
-// function onContainerClick(event) {
-//   event.preventDefault();
-
-//   if (event.target.classList.contains("gallery")) return;
-//     const source = event.target.dataset.source;
-    
-    
-   
-// const lightbox = new SimpleLightbox('.gallery a', {
-//     captionsData: 'alt',
-//     captionDelay: 250,
-//   });
-// }
-
-
 
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-// console.log(galleryItems);
 
 const galleryMarkUp = document.querySelector(".gallery");
 
