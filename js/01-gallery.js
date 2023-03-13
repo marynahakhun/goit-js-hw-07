e5b93c020c87c03945747c83c30a4e15b2f5c917
Maryna Hakhun <114681951+marynahakhun@users.noreply.github.com>
2023-03-13 15:04:39 +0100

    Add files via upload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,8 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const galleryContainerEl = document.querySelector(".gallery");
 const imagesMarkup = createItemsMarkup(galleryItems);
-
+let instance = null; 
 
 function createItemsMarkup(item) {
   return galleryItems
@@ -21,35 +20,32 @@ function createItemsMarkup(item) {
     })
     .join("");
 }
+
 const onContainerClick = (event) => {
   event.preventDefault();
 
   if (event.target.classList.contains("gallery")) return;
-    const source = event.target.dataset.source;
-    
-  const instance = basicLightbox.create(`
-    <img src="${source}"width="800" height="600">`);
+  const source = event.target.dataset.source;
+
+  instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`, {
+    onShow: () => {
+      document.addEventListener("keydown", onEscKeyPress);
+    },
+    onClose: () => {
+      document.removeEventListener("keydown", onEscKeyPress);
+    }
+  });
 
   instance.show();
-  document.addEventListener("keydown", onEscKeyPress);
-  function onEscKeyPress(event){
-    const isEscKey = event.code === "Escape"
-    if (isEscKey){
-        instance.close()
-    }
-}}
+};
+
+function onEscKeyPress(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
+
 
 galleryContainerEl.addEventListener("click", onContainerClick);
-
 galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
-
-
-
-
-
-
-console.log(galleryItems);
-
-
-
-
