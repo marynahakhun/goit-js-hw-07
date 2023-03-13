4ad40e452900bdb8e9e8af386e8c6026942c9fa0
Maryna Hakhun <114681951+marynahakhun@users.noreply.github.com>
2023-03-13 15:05:01 +0100

    Add files via upload

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,39 +1,72 @@
-import { galleryItems } from './gallery-items.js';
+// import { galleryItems } from './gallery-items.js';
 
-const galleryContainerEl = document.querySelector(".gallery");
-const imagesMarkup = createItemsMarkup(galleryItems);
+// const galleryContainerEl = document.querySelector(".gallery");
+// const imagesMarkup = createItemsMarkup(galleryItems);
 
-galleryContainerEl.addEventListener("click", onContainerClick);
-galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
+// galleryContainerEl.addEventListener("click", onContainerClick);
+// galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
 
-function createItemsMarkup(item) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-      <li>
-      <a class="gallery__item" href="${original}">
-      <img class="gallery__image" 
-      src="${preview}" 
-      alt="${description}" />
-        </a>
-      </li>`
+// function createItemsMarkup(item) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//       <li>
+//       <a class="gallery__item" href="${original}">
+//       <img class="gallery__image" 
+//       src="${preview}" 
+//       alt="${description}" />
+//         </a>
+//       </li>`
    
-    })
-    .join("");
-}
+//     })
+//     .join("");
+// }
 
-function onContainerClick(event) {
-  event.preventDefault();
+// function onContainerClick(event) {
+//   event.preventDefault();
 
-  if (event.target.classList.contains("gallery")) return;
-    const source = event.target.dataset.source;
+//   if (event.target.classList.contains("gallery")) return;
+//     const source = event.target.dataset.source;
     
     
    
-var lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
-}
+// const lightbox = new SimpleLightbox('.gallery a', {
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//   });
+// }
 
+
+
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// console.log(galleryItems);
+
+const galleryMarkUp = document.querySelector(".gallery");
+
+
+
+  const createItemsMarkup = galleryItems
+      .map(({ preview, original, description }) => {
+        return `
+        <li>
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+          </a>
+        </li>`
+     
+      })
+      .join("");
+  
+
+galleryMarkUp.insertAdjacentHTML("beforeend", createItemsMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
 
